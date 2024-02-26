1589e3799e9596ce47ce37d5dea86b01b32ffa99
Terrini <Haileykayy69@gmail.com>
2024-02-26 14:12:35 -0500

    Added ELIFS. blizzard andf Rainy VRS Function

--- GrimaceShake.py
+++ GrimaceShake.py
@@ -8,9 +8,23 @@ from time import sleep
 
 #Create a function that rnadomly chooses the weather from a list
 def weather():
-        weatherforcast = ["Snowing", "Blizzards", "Raining", "Foggy", "Windy", "icy", "sunny"]
+        weatherforcast = ["snowing", "blizzards", "raining", "foggy", "windy", "icy", "sunny"]
         weatherConditions = random.choice(weatherForecast)
-        print(weatherConditions)
         return weatherConditions
 
- print(weather())
+# Variable to call the weather() once VRS(Vehicle Response System) is determine
+ weatherAlert = weather()
+
+def vehicleResponseSystem():
+    if weatherAlert == "Snowing":
+     print"\nNational weather Service has updated our alarm by 30 minutes because of the forcast of",weatherAlert,
+          "weather conditions.")
+    print("VRS has been engaged only allowing you to drive 50mph.")
+    elif weatherAlert == "blizzard":\
+          print"\nNational weather Service has updated our alarm by 45 minutes because of the forcast of",weatherAlert,
+               "weather conditions.")
+         print("VRS has been engaged only allowing you to drive 40mph.")
+    elif weatherAlert == "blizzard":\
+        print"\nNational weather Service has updated our alarm by 10 minutes because of the forcast of",weatherAlert,
+            "weather conditions.")
+        print("VRS has been engaged only allowing you to drive 60mph.")
