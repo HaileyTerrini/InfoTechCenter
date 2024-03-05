ff8db3563b2678b48402bcba1049e7013d85a041
Terrini <Haileykayy69@gmail.com>
2024-03-05 14:23:43 -0500

    updated weather

--- GrimaceShake.py
+++ GrimaceShake.py
@@ -8,9 +8,8 @@ from time import sleep
 
 #Create a function that rnadomly chooses the weather from a list
 def weather():
-        weatherforcast = ["Snowing", "Blizzards", "Raining", "Foggy", "Windy", "icy", "sunny"]
-        weatherConditions = random.choice(weatherForecast)
-        print(weatherConditions)
+        weatherForcast = ["Snowing", "Blizzards", "Raining", "Foggy", "Windy", "icy", "sunny"]
+        weatherConditions = random.choice(weatherForcast)
         return weatherConditions
 
- print(weather())
+print(weather())
